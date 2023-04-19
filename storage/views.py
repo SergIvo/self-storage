5a72748a090d03da9d03c3e4ee0bc9fd1f8aa4ef
Maksim <106922768+Kalininmladshiy@users.noreply.github.com>
2023-04-19 11:30:19 +0300

    Added boxes

--- storage/views.py
+++ storage/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'index.html', context)
+
+
+def get_boxes(request):
+    context = {}
+    return render(request, 'boxes.html', context)
